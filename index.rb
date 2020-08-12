7569ed528f7232058ebc70450fd652120a2ed1d2
Kyle Trahan <ktrahan2@gmail.com>
2020-08-12 12:17:59 -0400

    8.12.2020 solution

--- index.rb
+++ index.rb
@@ -1,6 +1,24 @@
-#8.11.2020 Solution "name of method"
+require 'pry'
+
+#8.11.2020 Solution "change a parameter into a string"
 def numberToString(num)
     num.to_s
   end
 
+#8.12.2020 Solution "Sum of Numbers"
+def get_sum(a,b)
+  if a < b
+    sum_array = Array (a..b)
+    sum_array.reduce(0) do |sum, number|
+      sum += number
+    end
+  else  
+    sum_array = Array (b..a)
+    sum_array.reduce(0) do |sum, number|
+    sum += number
+    end  
+  end
+end
 
+    
+binding.pry
