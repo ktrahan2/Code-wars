795f60f0b627f5f8edf3c8366f078e6078da44f8
Kyle Trahan <ktrahan2@gmail.com>
2020-10-26 10:53:13 -0400

    first go code-wars

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("CODE-WARS")
+}
+
+// Solution 10.26.2020 "multiply" figure out why the function wasn't working
+// package multiply
+
+func Multiply(a, b int) int {
+	return a * b
+}
